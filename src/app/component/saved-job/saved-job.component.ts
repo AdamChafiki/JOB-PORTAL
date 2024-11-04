87297f04190e253eca923bc2d27fe1df33cc3553
Chafiki <adamchafiki3@gmail.com>
2024-11-04 13:22:41 +0100

    mod header auth

---
+++ src/app/component/saved-job/saved-job.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-job',
+  standalone: true,
+  imports: [],
+  templateUrl: './saved-job.component.html',
+})
+export class SavedJobComponent {
+
+}
