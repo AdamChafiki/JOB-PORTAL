838d54c321affcbabeb3a00d180004ae0a127c6b
Chafiki <adamchafiki3@gmail.com>
2024-11-07 19:21:38 +0100

    job-cop

--- src/app/shared/component/job-card/job-card.component.ts
+++ src/app/shared/component/job-card/job-card.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-job-card',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, DatePipe, TitleCasePipe],
   templateUrl: './job-card.component.html',
 })
 export class JobCardComponent {
-
+  title = "web developer"
 }
